0ff32648c1ea06634f194953b11ccf940f2e2c57
Kristyn Little <kristynlittle@frontier.com>
2021-03-20 03:00:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -8,9 +8,9 @@
 #end
 
 def prime?(value)
-  #if value <= 1 || value == 1 || value == 0
-    #return false
-  if
+  if value <= 1 || value == 1 || value == 0
+    return false
+  elsif
     (2..value - 1).each do |i|
       if value % i == 0
         return false
